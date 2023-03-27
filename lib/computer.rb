39d8b1717c72081b79b3c1aba7e4154f1e29a556
D.J. Crosby <taladan@gmail.com>
2023-03-27 19:05:03 -0400

    Create guess method
    Guess method handles the logic of obtaining a solve through creating
    a solver object, then sending its initial guess into the feedback call.
    Once it has the initial feedback from that call against the set cypher,
    it calls the solver to reduce the solution pool and give feedback until
    it reaches a single length solution which should match the cypher.
    
    This method returns a two (2) length array containing a stringified
    version of the solution as well as the number of turns that it took
    to reach this solution.
    
    It doesn't always get the answer in under 5 steps, but most of the
    test runs I've put it through it seems to hit between 4 and 6 guesses
    fairly consistantly.

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,5 +1,5 @@
 # lib/computer.rb
-require_relative "state"
+require_relative "solver"
 require "pry-byebug"
 
 #
@@ -30,14 +30,39 @@ class Computer
         ("2" * (config[:cypher_length] - (config[:cypher_length] / 2)))
   end
 
-  def guess(config, code)
-    guess = 
+  def guess(code)
+    guess = @initial_guess
+    pool = @guess_pool.clone
+    solver = Solver.new(pool, code)
+
+    turn = 0
+    while pool.length > 1 && turn < @config[:total_attempts]
+      feedback = solver.get_feedback(guess, code)
+      if feedback[:cp] < code.length
+        pool = solver.reduce_solutions(guess, feedback, pool)
+        if pool.length > 2
+          guess = pool.last
+        elsif pool.length == 2
+          if pool[0] == code
+            pool = pool[0]
+          else
+            pool = pool[1]
+          end
+          break
+        end
+        turn += 1
+      elsif feedback[:cp] == code.length
+        pool = guess
+        break
+      end
+    end
+    pool = pool.join if pool.is_a?(Array)
+    result = [pool, turn + 1]
   end
 
   private
 
   def create_pool()
-    binding.pry
     base_pool = %w[1 2 3 4 5 6]
 
     @config[:allow_blanks] ? base_pool.push(" ") : base_pool
@@ -50,88 +75,3 @@ class Computer
     pool
   end
 end
-
-#   def master_mind(length, mastermind_code)
-#     if length == 2
-#       use_pool = @guess_pool[:cart_2]
-#     elsif length == 3
-#       use_pool = @guess_pool[:cart_3]
-#     elsif length == 4
-#       use_pool = @guess_pool[:cart_4]
-#     elsif length == 5
-#       use_pool = @guess_pool[:cart_5]
-#     else
-#       use_pool = @guess_pool[:cart_6]
-#     end
-
-#     total_codes = use_pool
-#     knuth_codes = total_codes
-#     possible_codes = total_codes
-#     total_guesses = 0
-#     win_state = false
-
-#     while win_state == false
-#       code = get_code(knuth_codes, possible_codes)
-#       # delete this and the feedback puts when complete
-#       feedback = State.computer_check_positions(code, mastermind_code)
-#       # Need to figure out how to integrate the returned values of positioning into this
-#       if feedback == (cp == length && wp == 0)
-#         total_guesses += 1
-#         win_state = true
-#         break
-#       else
-#         prune_list(code, feedback, knuth_codes)
-#         total_guesses += 1
-#       end
-#     end
-#     total_guesses
-#   end
-
-#   def get_code(knuth_codes, possible_codes)
-#     guess_codes = mini_max(knuth_codes, possible_codes)
-#     code = get_guess_code_from_list(knuth_codes, guess_codes)
-#     possible_codes.delete(code)
-#     code
-#   end
-
-#   def mini_max(knuth_codes, possible_codes)
-#     times_found = Hash.new(0)
-#     scores = Hash.new(0)
-#     guess_codes = []
-#     possible_codes.each do |code|
-#       knuth_codes.each do |code_to_crack|
-#         # here's where we need State.check_positions
-#         feedback = State.computer_check_positions(code_to_crack, code)
-#         times_found[feedback] += 1
-#       end
-#       maximum = times_found.max_by { |key, value| value }
-#       scores[code] = maximum
-#     end
-#     puts scores
-#     minimum = scores.min_by { |key, value| value }
-
-#     possible_codes.each do |code|
-#       guess_codes.push(code) if scores[code] == minimum
-#     end
-#     guess_codes
-#   end
-
-#   def prune_list(last_guess, feedback, knuth_codes)
-#     knuth_codes.each do |code|
-#       retrieved_feedback = State.computer_check_positions(code, last_guess)
-#       knuth_codes.delete(code) if retrieved_feedback != feedback
-#     end
-#   end
-
-#   def get_guess_code_from_list(knuth_codes, guess_codes)
-#     found_code = nil
-#     knuth_codes.each do |code|
-#       if guess_codes.include?(code)
-#         found_code = code
-#         break
-#       else
-#         guess_codes.first()
-#       end
-#     end
-#   end
-# end
