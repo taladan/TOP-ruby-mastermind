ac51d7715aebc49371e0a87ade106509a434dc65
D.J. Crosby <taladan@gmail.com>
2023-03-29 12:29:09 -0400

    Implement class for gameplay and configuration
    After some suggestions from Lofty on TOP discord, I decided
    to break out the game configuration logic into its own class.
    
    This code is currently functional

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -1,8 +1,46 @@
 # lib/mastermind.rb
 #
+require "./lib/cypher"
+require "./lib/player"
+require "./lib/computer"
+require "./lib/prompt"
 
 class Mastermind
+  attr_reader :player, :code
   def initialize
-    ask = Prompt.new()
+    @ask = Prompt.new()
+    configure_gameplay()
+  end
+
+  private
+
+  def configure_gameplay()
+    player_name = @ask.query("intro")
+    @player = Player.new(@player_name)
+
+    @player.config[:allow_duplicates] = @ask.query("duplicates")
+    @player.config[:blanks] = @ask.query("blanks")
+    @player.config[:total_attempts] = @ask.query("attempts")
+    @player.config[:cypher_length] = @ask.query("length")
+    @player.config[:role] = @ask.query("role")
+
+    # generate code & set
+    @code =
+      Cypher.new(
+        @player.config[:allow_duplicates],
+        @player.config[:allow_blanks],
+        @player.config[:cypher_length],
+      )
+    @player.config[:cypher] = get_code(@player.config[:role])
+  end
+
+  def get_code(role)
+    # options[:turns]
+    if role == "breaker"
+      @code.breaker_generate
+    else
+      @code.maker_generate
+    end
+    @code.cypher
   end
 end
